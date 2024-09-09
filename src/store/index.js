1047f3fcbbc37e1da6dbb7c8072f3e53b5aeac06
ms-tech-geek <thecorporator@gmail.com>
2024-09-09 12:44:26 +0530

    attaching payloads to actions

--- src/store/index.js
+++ src/store/index.js
@@ -8,6 +8,8 @@ const counterReducer = (state = initialData, action) => {
 	switch (action.type) {
 		case "INCREMENT":
 			return { counter: state.counter + 1 };
+		case "INCREASE":
+			return { counter: state.counter + action.amount };
 		case "DECREMENT":
 			return { counter: state.counter - 1 };
 		default:
