23ed4150b6acb0c047d649b591ee5bc8cfb971d1
ms-tech-geek <thecorporator@gmail.com>
2024-09-09 16:57:41 +0530

    working with multiple components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import Counter from './components/Counter';
-
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import Counter from "./components/Counter";
 
 function App() {
-  return (
-    <Counter />
-  );
+	return (
+		<>
+			<Header />
+			<Auth />
+			<Counter />
+		</>
+	);
 }
 
 export default App;
